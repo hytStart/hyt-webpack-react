897a8cebda3c36df0b0f4ac65322464fd13f5a45
YiTing Huang <huangyiting@sunlands.com>
2019-06-17 15:25:18 +0800

    style-loader css-loader extract-text-plugin file-loader url-loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+import './style/index.css'
+import bg from './images/scene.png'
+
 function component() {
     var element = document.createElement('div')
 
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = 'asdfdasfdasfdasfdsafdasfafsdfdsasdfafsda'
+    element.classList.add('hello')
+
+    var myIcon = new Image()
+    myIcon.src = bg
+    element.appendChild(myIcon)
 
     return element
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,42 @@
 const path = require('path')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.[hash].js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader"],
+                }),
+            },
+            {
+                test: /\.(png|jpg|gif|eot|svg|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                        }
+                    },
+                ],
+            },
+            {
+                test: /\.(mp4|ogg)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css"),
+    ]
 }
